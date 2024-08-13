1b91ec5d23664594e105f44594a0ce030892874f
Misha Tchanturidze <tchanturidzemikheili@gmail.com>
2024-08-13 11:29:06 +0400

    add contacts page

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {LanguageClassDirective} from "../../shared/directives/language-class.directive";
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [LanguageClassDirective, TranslateModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
