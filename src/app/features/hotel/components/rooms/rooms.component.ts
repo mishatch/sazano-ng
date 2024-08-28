1f4e6f858c4cf42574139628866f2eb5f2bd16e1
Misha Tchanturidze <tchanturidzemikheili@gmail.com>
2024-08-29 02:32:57 +0400

    add loading component

--- src/app/features/hotel/components/rooms/rooms.component.ts
+++ src/app/features/hotel/components/rooms/rooms.component.ts
@@ -1,27 +1,37 @@
 import { Component, ViewChild } from '@angular/core';
 import { RoomDetailsComponent } from './room-details/room-details.component';
 import { RoomService } from '../../services/room.service';
-import { NgForOf } from "@angular/common";
-import {translate} from "@angular/localize/tools";
-import {TranslateModule} from "@ngx-translate/core";
-import {LanguageClassDirective} from "../../../../shared/directives/language-class.directive";
+import {NgForOf, NgIf} from "@angular/common";
+import { TranslateModule } from "@ngx-translate/core";
+import { LanguageClassDirective } from "../../../../shared/directives/language-class.directive";
+import { LoadingComponent } from "../../../../shared/components/loading/loading.component";
 
 @Component({
   selector: 'app-rooms',
   standalone: true,
-  imports: [RoomDetailsComponent, NgForOf, TranslateModule, LanguageClassDirective],
+  imports: [RoomDetailsComponent, NgForOf, TranslateModule, LanguageClassDirective, LoadingComponent, NgIf],
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
 export class RoomsComponent {
   rooms: any[] = [];
+  isLoading = true;
 
   @ViewChild('roomDetailsModal') roomDetails!: RoomDetailsComponent;
 
   constructor(private roomService: RoomService) {
+    this.fetchRooms();
+  }
+
+  fetchRooms() {
+    this.isLoading = true;
     this.roomService.getRooms().subscribe((data: any) => {
       this.rooms = data;
+      this.isLoading = false;
       console.log(this.rooms);
+    }, error => {
+      this.isLoading = false;
+      console.error('Error fetching rooms:', error);
     });
   }
 
@@ -30,5 +40,4 @@ export class RoomsComponent {
     this.roomDetails.description = description;
     this.roomDetails.openModal();
   }
-
 }
